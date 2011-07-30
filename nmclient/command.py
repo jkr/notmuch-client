e4b382e4520d3f6b2408c8420347e3f999e208ae
Jesse Rosenthal <jrosenthal@jhu.edu>
2011-07-29 23:35:32 -0400

    Added copyright statements to each of the code files.

--- nmclient/command.py
+++ nmclient/command.py
@@ -1,3 +1,26 @@
+# -*- python;  coding: utf-8  -*-
+#########################################################################
+# command.py: classes for intercepting and possibly altering notmuch    #
+# commands                                                              #
+#                                                                       #
+# Copyright © 2011 Jesse Rosenthal                                      #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see http://www.gnu.org/licenses/ .  #
+#                                                                       #
+# Author: Jesse Rosenthal <jrosenthal@jhu.edu>                          #
+#########################################################################
+
 from subprocess import Popen, PIPE
 from nmclient.shared import _run_command_locally, _run_command_over_ssh
 from nmclient.dates import DateRange
--- nmclient/crypto.py
+++ nmclient/crypto.py
@@ -1,3 +1,25 @@
+# -*- python;  coding: utf-8  -*-
+#########################################################################
+# crypto.py: functions for use with encryption and verification         #
+#                                                                       #
+# Copyright © 2011 Jesse Rosenthal                                      #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see http://www.gnu.org/licenses/ .  #
+#                                                                       #
+# Author: Jesse Rosenthal <jrosenthal@jhu.edu>                          #
+#########################################################################
+
 from nmclient.bundled.gnupg import gnupg
 import tempfile
 import os
